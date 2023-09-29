d1557e1ee4eca19f8f7ebb4ce38459b06fd2a06d
Manvinderjit <91133409+manvinderjit@users.noreply.github.com>
2023-09-29 18:02:37 -0400

    feat: Add new message

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: "Mini Messageboard", messages: messages });
 });
 
+/* GET new message page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: "Add New Message"});
+});
+
+/* POST new message page. */
+router.post('/new', function(req, res, next) {
+  messages.push({text: req.body.messageText, user: req.body.messageUser, added: new Date()});
+  res.redirect('/');
+});
+
 module.exports = router;
